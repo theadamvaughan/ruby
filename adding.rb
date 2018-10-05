d3aaeec2a366e809e4ab90fcb12ee941e37f5dbc
Adam Vaughan <adam_vaughan1982@hotmail.com>
2018-10-05 16:22:40 +0100

    updated code

--- adding.rb
+++ adding.rb
@@ -1,8 +1,8 @@
 puts "I'm going to do some math!"
 puts "Can I have your first number?"
-num1 = gets.chomp
+num1 = gets.to_i
 puts "Thanks! Can I have your second number?"
-num2 = gets.chomp
+num2 = gets.to_i
 
 print "#{num1} plus #{num2} equals " 
-print (num1.to_i + num2.to_i)
+print (num1 + num2)
