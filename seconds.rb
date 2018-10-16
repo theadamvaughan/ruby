d1e8b0bae67790c14bd895db90ae1afc57b4e654
Adam Vaughan <adam_vaughan1982@hotmail.com>
2018-10-16 20:19:24 +0100

    tidied up code

--- seconds.rb
+++ seconds.rb
@@ -1,8 +1,8 @@
-puts "What is your name?"
+puts 'What is your name?'
 name = gets.chomp
-puts "How old are you?"
+puts 'How old are you?'
 age = gets.to_i
 
-age1 = age * 31536000
+age1 = age * 31_536_000
 
 puts "Hi #{name}! you are approx #{age1} seconds old!"
