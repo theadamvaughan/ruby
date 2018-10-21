5a6baefe5cc11c75ea55947daff6b01f0d26ef58
Adam Vaughan <adam_vaughan1982@hotmail.com>
2018-10-21 19:30:25 +0100

    added equation to work out rought age

--- theBigExercise.rb
+++ theBigExercise.rb
@@ -1,20 +1,22 @@
-puts Time.now.strftime('%d/%m/%Y')
+date = Time.now.strftime('%d/%m/%Y')
 
 puts 'Can I have your full name?'
   name = gets.chomp
 
-puts'Can I have your age?'
-  age = gets.chomp.to_i
+puts 'Can I have your year of birth? (YYYY)'
+  yob = gets.chomp.to_i
 
-if age <= 16
-   puts 'Has your parent given you permission to use this programme? (yes or no)'
-   answer = gets.chomp
+age = 2018 - yob
+
+if age <= 15
+  puts 'Has your parent given you permission to use this programme? (yes or no)'
+  answer = gets.chomp
 end
 if answer == 'yes' && (age <= 6) || (age >= 70)
-   puts "Well done #{name}! Look at you using a computer!"
+  puts "Well done #{name}! Look at you using a computer!"
 
 elsif answer == 'no'
   puts "Sorry #{name}, but you are not authorised to use this programme"
 else
-   puts "Your name is #{name} and you are #{age} years old"
+  puts "Your name is #{name} and you are #{age} years old"
 end
