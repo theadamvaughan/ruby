dd79acdfdfae3cc6e1685f723b3393e61d393da7
Adam Vaughan <adam_vaughan1982@hotmail.com>
2018-11-03 16:05:49 +0000

    Finally got it to work!!

--- theBigExercise.rb
+++ theBigExercise.rb
@@ -1,12 +1,22 @@
-date = Time.now.strftime('%d/%m/%Y')
+require 'date'
+
+today = DateTime.now
 
 puts 'Can I have your full name?'
-  name = gets.chomp
+name = gets.chomp
 
-puts 'Can I have your year of birth? (YYYY)'
-  yob = gets.chomp.to_i
+puts "Can I have the day you were born?"
+day = gets.chomp.to_i
 
-age = 2018 - yob
+puts "Can I have the month you were born?"
+month = gets.chomp.to_i
+
+puts "Can I have your year of birth?"
+year = gets.chomp.to_i
+
+today = Date.today
+d = Date.new(today.year, month, day)
+age = d.year - year - (d > today ? 1 : 0)
 
 if age <= 15
   puts 'Has your parent given you permission to use this programme? (yes or no)'
